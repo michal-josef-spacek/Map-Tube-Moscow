10dfb4c009f8220fae5ee3077fe5bbe55358d64d
Michal Josef Špaček <tupinek@gmail.com>
2023-12-19 12:44:18 +0100

    Improve SYNOPSIS section in doc

--- Moscow.pm
+++ Moscow.pm
@@ -33,6 +33,7 @@ Map::Tube::Moscow - Interface to the Moscow Metro Map.
 =head1 SYNOPSIS
 
  use Map::Tube::Moscow;
+
  my $obj = Map::Tube::Moscow->new;
  my $routes_ar = $obj->get_all_routes($from, $to);
  my $line = $obj->get_line_by_id($line_id);
